d6a99467f0301aebf71df7e94778ceb4826c159e
sadiq <sadiq@emperorianinteractive.com>
2026-08-07 14:14:39 +0100

    portal service

--- src/routes/service_routes.ts
+++ src/routes/service_routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { initializePayment, createAdmissionRecord } from "../serviceController/mutation";
+import { initializePayment, createAdmissionRecord, loginStudent } from "../serviceController/mutation";
 import { getAdmissionRecord, verifyPayment } from "../serviceController/queries";
 
 const router = express.Router();
 
+router.post("/auth/login", loginStudent);
 router.post("/payments/initialize", initializePayment);
 router.get("/payments/verify/:reference", verifyPayment);
 router.post("/admission", createAdmissionRecord);
